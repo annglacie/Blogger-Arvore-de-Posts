37ba135b11887bdad2cba4156cbe99454eb588d8
annglacie <danielledsigner@gmail.com>
2016-03-12 02:36:33 -0300

    Update Make-Blogger-Archive-Page.js

--- Make-Blogger-Archive-Page.js
+++ Make-Blogger-Archive-Page.js
@@ -32,7 +32,7 @@ function LoadTheArchive(TotalFeed)
 
 function DisplaytheTOC(PostTitles,PostURLs,PostYears,PostMonths,PostDays)
 {
-    var MonthNames=["January","February","March","April","May","June","July","August","September","October","November","December"];
+    var MonthNames=["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Augosto","Setembro","Outubro","Novembro","Dezembro"];
     var NumberOfEntries=PostTitles.length;
     for(var EntryNum = 0; EntryNum < NumberOfEntries; EntryNum++)
     {
